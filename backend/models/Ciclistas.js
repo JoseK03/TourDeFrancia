f83900fc3e0174fd429051ec5b8c91bea09d0393
JoseK <dajoseva@gmail.com>
2023-07-12 09:28:32 -0500

    Ciclistas.js

--- backend/models/Ciclistas.js
+++ backend/models/Ciclistas.js
@@ -2,7 +2,24 @@ import mongoose from "mongoose";
 
 const CiclistaSchema = mongoose.Schema({
     nombre:{
-        
+        type:String,
+        required:true,
+        trim:true
+    },
+    edad:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    estatura:{
+        type:String,
+        required:true,
+        trim:true,
     }
-    ,edad,estatura
-})
+    },
+    {
+        timestamps: true
+    }
+    );
+    const Ciclista = mongoose.model("ciclista",CiclistaSchema);
+    export default CiclistaSchema;
