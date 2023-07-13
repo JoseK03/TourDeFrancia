b563061aec043d92d2f11b7970049cf5863d1b21
JoseK03 <dajoseva@gmail.com>
2023-07-13 15:24:48 -0500

    :fire:

--- backend/controllers/ciclistas.controllers.js
+++ backend/controllers/ciclistas.controllers.js
@@ -1,7 +1,7 @@
 import Ciclista from "../models/Ciclistas.js";
 
 const obtenerCiclistas = async (req,res) =>{
-    const ciclista = Ciclista.find();
+    const ciclista = await Ciclista.find();
     res.json(ciclista)
 }
 
@@ -19,9 +19,27 @@ const deleteCiclista = async (req,res) => {
     try {
         await Ciclista.deleteOne({_id:req.params.id});
         res.status(204).send();
-
     } catch (error) {
-        
+        console.log(error);
+    }
+}
+
+const updateCiclista = async (req,res) => {
+    try {
+        const ciclista = await Ciclista.deleteOne({_id:req.params.id});
+        if(req.body.nombre){
+            ciclista.nombre = req.body.nombre
+        }
+        if(req.body.edad){
+            ciclista.edad = req.body.edad
+        }
+        if(req.body.estatura){
+            ciclista.estatura = req.body.estatura
+        }
+        await ciclista.save();
+        res.send(ciclista);
+    } catch (error) {
+        console.log(error);
     }
 }
 
