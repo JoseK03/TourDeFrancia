a5e0b93bc5ce830c18203134c8e52bbaa15e30ee
JoseK03 <dajoseva@gmail.com>
2023-07-13 15:24:28 -0500

    :fire:

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import conectarDB from "./config/config.js";
+import routerCiclistas from "./routes/ciclistas.routes.js";
+import routerGrupos from "./routes/grupos.routes.js";
 
 const app = express();
 dotenv.config();
@@ -9,6 +11,11 @@ app.use(express.json());
 
 const PORT = process.env.PORT;
 conectarDB();
+
+app.use("/ciclistas",routerCiclistas);
+app.use("/equipos",routerGrupos);
+
+
 app.listen(PORT,()=>{
     console.log(`si sirve el puerto ${PORT}`);
 })
